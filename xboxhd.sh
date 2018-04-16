09df6d54cc6000a6abba2c3d3f83c8e80c5af47f
Parker Reed <parker.l.reed@gmail.com>
2018-04-16 14:12:27 -0400

    Add proper Qemu arguments to fix deprecation warnings

--- xboxhd.sh
+++ xboxhd.sh
@@ -2,7 +2,7 @@
 hdd_path=""
 lockhdd() { ./smartctl -g security $1; ./smartctl -s security-eeprom-setpass,eeprom.bin $1; }
 unlockhdd() { ./smartctl -g security $1; ./smartctl -s security-eeprom-unlock,eeprom.bin $1; ./smartctl -s security-eeprom-disable,eeprom.bin $1; }
-qemu_run() { qemu-system-i386 -kernel kernel -initrd initrd.gz -hdb fat:hdm/ -hda $1 -append "load_ramdisk=1 prompt_ramdisk=0 ramdisk_size=24000 rw root=/dev/ram pci=biosirq vga=0x317"; }
+qemu_run() { qemu-system-i386 -kernel kernel -initrd initrd.gz -drive file=fat:rw:hdm/,index=1,format=raw -drive file=$1,index=0,media=disk,format=raw -append "load_ramdisk=1 prompt_ramdisk=0 ramdisk_size=24000 rw root=/dev/ram pci=biosirq vga=0x317"; }
 select_hdd() { ( readarray -t lines < <(lsblk -I 8 -dno NAME,SIZE,MODEL); select choice in "${lines[@]}"; do [[ -n $choice ]] || { echo "Invalid choice. Please try again." >&2; continue; }; break; done; read -r hdd_path size unused <<<"$choice"; echo $hdd_path; ) }
 
 if [ "$EUID" -ne 0 ]
